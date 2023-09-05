2721e5be26e13d2a22fb09f6dd7daea522e14b00
Mariengca27 <mariengca27>
2023-09-05 10:58:18 -0300

    Projeto Estoque, gitignore, DAL e DLL

---
+++ WebSite1/App_Code/DAL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descrição resumida de DAL
+/// </summary>
+public class DAL
+{
+    public DAL()
+    {
+        //
+        // TODO: Adicionar lógica do construtor aqui
+        //
+    }
+}
